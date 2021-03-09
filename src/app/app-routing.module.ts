1314590e5ea8441c9ac1c72e6b32556c1b54cf1f
Sefkan <tas.sefkan@gmail.com>
2021-03-09 20:15:47 +0100

    some module and routing stuff :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './components/connexion/connexion.component';
 import { HomeComponent } from './components/home/home.component';
-import { PcAccessoiresComponent } from './components/pc-accessoires/pc-accessoires.component';
-import { PcBureauComponent } from './components/pc-bureau/pc-bureau.component';
-import { PcPortableComponent } from './components/pc-portable/pc-portable.component';
+import { PcAccessoiresComponent } from './components/ordinateur/pc-accessoires/pc-accessoires.component';
+import { PcBureauComponent } from './components/ordinateur/pc-bureau/pc-bureau.component';
+import { PcPortableComponent } from './components/ordinateur/pc-portable/pc-portable.component';
 
 const routes: Routes = [
   {path:"", component: HomeComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { PcPortableComponent } from './components/pc-portable/pc-portable.component';
-import { PcBureauComponent } from './components/pc-bureau/pc-bureau.component';
-import { PcAccessoiresComponent } from './components/pc-accessoires/pc-accessoires.component';
+import { PcPortableComponent } from './components/ordinateur/pc-portable/pc-portable.component';
+import { PcBureauComponent } from './components/ordinateur/pc-bureau/pc-bureau.component';
+import { PcAccessoiresComponent } from './components/ordinateur/pc-accessoires/pc-accessoires.component';
 import { ConnexionComponent } from './components/connexion/connexion.component';
 
 @NgModule({
