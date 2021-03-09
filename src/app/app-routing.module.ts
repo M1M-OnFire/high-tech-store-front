7e4ca0a7d7c38b6b62a60b8eb36f6d9b445570f3
Sefkan <tas.sefkan@gmail.com>
2021-03-09 20:13:33 +0100

    add some components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { HomeComponent } from './components/home/home.component';
+import { PcAccessoiresComponent } from './components/pc-accessoires/pc-accessoires.component';
+import { PcBureauComponent } from './components/pc-bureau/pc-bureau.component';
+import { PcPortableComponent } from './components/pc-portable/pc-portable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"pc-portable", component: PcPortableComponent},
+  {path:"pc-bureau", component: PcBureauComponent},
+  {path:"pc-accessoires", component: PcAccessoiresComponent},
+  {path:"pc-connexion", component: ConnexionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PcPortableComponent } from './components/pc-portable/pc-portable.component';
+import { PcBureauComponent } from './components/pc-bureau/pc-bureau.component';
+import { PcAccessoiresComponent } from './components/pc-accessoires/pc-accessoires.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    PcPortableComponent,
+    PcBureauComponent,
+    PcAccessoiresComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
