044f52744bc421b3cdfeabb7f8cefffcb7f31ad1
Sefkan <tas.sefkan@gmail.com>
2021-03-22 23:00:20 +0100

    bouton affiche pas affiche si connecte pas connecte :)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,7 +9,10 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor (private authService:AuthService, private router:Router) {
+  constructor (
+      public authService:AuthService,
+      private router:Router,
+    ) {
   }
 
   logout() {
